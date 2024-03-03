6a901b1da0f5b89c7ade7c7042483ae8bdf7f0dd
bbq27 <133260826+bbq27@users.noreply.github.com>
2024-03-03 11:18:09 -0800

    added stuff

--- backend/verification.py
+++ backend/verification.py
@@ -10,13 +10,38 @@ def get_database():
    client = MongoClient(CONNECTION_STRING)
  
    # Create the database for our example (we will use the same database throughout the tutorial
-   return client['user_shopping_list']
+   return client['user_information']
   
 # This is added so that many files can reuse the function get_database()
 if __name__ == "__main__":   
   
    # Get the database
    dbname = get_database()
+   users = dbname['user_1_items']
+
+
+
+
+# add this stuff to end
+# item_1 = {
+#   "_id" : "U1IT00001",
+#   "item_name" : "Blender",
+#   "max_discount" : "10%",
+#   "batch_number" : "RR450020FRG",
+#   "price" : 340,
+#   "category" : "kitchen appliance"
+# }
+
+# item_2 = {
+#   "_id" : "U1IT00002",
+#   "item_name" : "Egg",
+#   "category" : "food",
+#   "quantity" : 12,
+#   "price" : 36,
+#   "item_description" : "brown country eggs"
+# }
+# collection_name.insert_many([item_1,item_2])
+
 
 
 # function checks if email ends in calpoly.edu
