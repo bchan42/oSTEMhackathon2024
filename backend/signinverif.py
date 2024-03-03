d786f4fa3ff8e0ae91cddee3090c6ea6b23a23b7
Bernette Chan <bernettechan@Bernettes-MacBook-Pro.local>
2024-03-03 13:03:55 -0800

    check fns

--- backend/signinverif.py
+++ backend/signinverif.py
@@ -3,3 +3,14 @@
 # not account w existing email
 # authenticate email exists in database
 # checks if password is right 
+
+# create some global dictionary
+user1 = {
+    'name': 'Bern',
+    'email': 'bern@gmail.com',
+    'password': 'Abc@34we'
+}
+
+def already_exists(user_info):
+    # if user_info.key() == 
+    return 'Bern' in user_info.values()
