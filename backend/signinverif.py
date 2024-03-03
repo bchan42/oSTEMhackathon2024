fe4133c61dc37c195e287034379a3110d72540ea
Bernette Chan <bernettechan@Bernettes-MacBook-Pro.local>
2024-03-03 11:37:42 -0800

    verif

---
+++ backend/signinverif.py
@@ -0,0 +1,5 @@
+# sign in verification
+
+# not account w existing email
+# authenticate email exists in database
+# checks if password is right 
--- backend/signupverif.py
+++ backend/signupverif.py
@@ -1,25 +1,19 @@
 # sign up verification
 
 from pymongo import MongoClient
+
 def get_database():
- 
    # Provide the mongodb atlas url to connect python to mongodb using pymongo
    CONNECTION_STRING = "mongodb+srv://user:pass@cluster.mongodb.net/myFirstDatabase"
- 
    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
    client = MongoClient(CONNECTION_STRING)
- 
    # Create the database for our example (we will use the same database throughout the tutorial
    return client['user_information']
   
 # This is added so that many files can reuse the function get_database()
 if __name__ == "__main__":   
-  
    # Get the database
    dbname = get_database()
-   users = dbname['user_1_items']
-
-
 
 
 # add this stuff to end
@@ -43,33 +37,23 @@ if __name__ == "__main__":
 # collection_name.insert_many([item_1,item_2])
 
 
-
 # function checks if email ends in calpoly.edu
 def check_calpoly(email):
     domain = '@calpoly.edu'
     parts = email.split('@')
     return len(parts) == 2 and parts[1].lower() == domain
  
-# function checks if password is less than 
- def check_password(password):
-    if len(password) < 8: # at least 8 characters
+# function checks if password
+def check_password(password):
+    if len(password) < 8:  # at least 8 characters
         print('Password needs to be at least 8 characters long.')
-    elif 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' not in password: # 1 capital letter
+    elif not any(char.isupper() for char in password):  # 1 capital letter
         print('Password needs to have a capital letter.')
-    elif 'abcdefghijklmnopqrstuvwxyz' not in password: # 1 lowercase
-        print('Password needs to have lowercase letter.')
-    elif not any(char.isdigit for char in password)# 1 number
+    elif not any(char.islower() for char in password):  # 1 lowercase
+        print('Password needs to have a lowercase letter.')
+    elif not any(char.isdigit() for char in password):  # 1 number
         print('Password needs to have a number.')
-    # 1 unique character
-    elif '1@#456&*()' not in password:
+    elif not any(char in '!@#$%^&*()' for char in password):  # 1 special character
         print('Password needs to have a special character.')
-
-
-
-# not account w existing email
-# authenticate email exists in database
-# checks if password is right 
-
-# give client rand generated token
-
-
+    else:
+        print('Password meets the criteria.')
